4ba097d0aff03dd3d1a765c9902b74d6cc27fca8
Benjamin <bwg@tdx.li>
2024-10-08 13:31:15 -0400

    Update pricelist.h with insert and update functions

--- include/pricelist.h
+++ include/pricelist.h
@@ -7,19 +7,24 @@
 typedef struct PricelistData* PricelistDataPtr;
 
 PricelistDataPtr pricelist_create(void);
+
 void pricelist_set_name(PricelistDataPtr pricelist, const char* name);
 void pricelist_set_is_default(PricelistDataPtr pricelist, bool is_default);
 void pricelist_set_active(PricelistDataPtr pricelist, bool active);
 void pricelist_set_use_prices(PricelistDataPtr pricelist, bool use_prices);
-bool pricelist_insert(PGconn *db_conn, PricelistDataPtr pricelist);
-int pricelist_get_id(PricelistDataPtr pricelist);
-void pricelist_free(PricelistDataPtr pricelist);
 
-void pricelist_add_item(PricelistDataPtr pricelist, int product_id, double price, bool active, 
-                        const char* client_code, const char* manufacture_id, 
-                        const char* date_available_from, const char* date_available_to, 
+void pricelist_add_item(PricelistDataPtr pricelist, const char* product_code, const char* product_name,
+                        double price, bool active, const char* client_code, const char* client_name,
+                        const char* manufacture_id, const char* date_available_from, const char* date_available_to, 
                         int min_quantity, int max_quantity);
 
-bool pricelist_fetch_and_insert(PGconn *db_conn);
+bool pricelist_fetch_and_insert(PGconn *db_conn, long last_processed_id);
+bool pricelist_insert(PGconn *db_conn, PricelistDataPtr pricelist);
+bool pricelist_update(PGconn *db_conn, PricelistDataPtr pricelist);
+
+
+int pricelist_get_id(PricelistDataPtr pricelist);
+
+void pricelist_free(PricelistDataPtr pricelist);
 
 #endif // PRICELIST_H
